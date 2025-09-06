ebde1f8b0367c2f6b8fd9eae72589f4c5b276248
d-manuel <38905215+d-manuel@users.noreply.github.com>
2025-09-06 10:36:34 +0200

    feat: press Q to quit oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,26 +1,27 @@
 return {
 	enabled = true,
-  "stevearc/oil.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({
-      columns = { "icon" },
-      keymaps = {
-        ["<C-h>"] = false,
-        ["<M-h>"] = "actions.select_split",
-        ["<C-s>"] = false,
-        ["<M-s>"] = "actions.select_vsplit",
-      },
-      view_options = {
-        show_hidden = true,
-      },
-      -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-      delete_to_trash = true,
-    })
-    -- Open parent directory in current window
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil: Open parent directory" })
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			columns = { "icon" },
+			keymaps = {
+				["<C-h>"] = false,
+				["<M-h>"] = "actions.select_split",
+				["<C-s>"] = false,
+				["<M-s>"] = "actions.select_vsplit",
+				["<S-q>"] = { "actions.close", mode = "n" },
+			},
+			view_options = {
+				show_hidden = true,
+			},
+			-- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
+			delete_to_trash = true,
+		})
+		-- Open parent directory in current window
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil: Open parent directory" })
 
-    -- Open parent directory in floating window
-    vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+		-- Open parent directory in floating window
+		vim.keymap.set("n", "<space>-", require("oil").toggle_float)
 	end,
 }
