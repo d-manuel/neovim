046e7aff48d9ef470ca4e7c7afdb811e98d30275
d-manuel <38905215+d-manuel@users.noreply.github.com>
2026-02-05 21:12:07 +0100

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,13 @@ return {
 	{ "neanias/everforest-nvim", },
 	{ "rebelot/kanagawa.nvim" },
 	{ "thesimonho/kanagawa-paper.nvim" },
+	{ "p00f/alabaster.nvim" },
 	{
 		"comfysage/evergarden",
 		lazy = false,
 		priority = 1000, -- High priority ensures it's loaded early before bufferline so bufferline can adapt the colors
 		config = function()
-			vim.cmd.colorscheme("everforest")
+			vim.cmd.colorscheme("alabaster")
 		end,
 	},
 }
